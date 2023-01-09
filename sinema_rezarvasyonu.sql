f8e01adf4403be2d02f231972bb2de04cadedbd2
hande <handenuragbulut@gmail.com>
2023-01-09 12:50:19 +0300

    send github

---
+++ sinema_rezarvasyonu.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 17 Ara 2019, 21:08:22
+-- Sunucu sürümü: 10.4.10-MariaDB
+-- PHP Sürümü: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `deneme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `sinema_rezarvasyonu`
+--
+
+CREATE TABLE `sinema_rezarvasyonu` (
+  `id` int(11) NOT NULL,
+  `salon_id` int(11) NOT NULL,
+  `seans_id` varchar(5) NOT NULL,
+  `koltuk_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `sinema_rezarvasyonu`
+--
+ALTER TABLE `sinema_rezarvasyonu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `sinema_rezarvasyonu`
+--
+ALTER TABLE `sinema_rezarvasyonu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
